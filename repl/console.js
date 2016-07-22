2f0592f9d78760ce6ca4a81c31f6199869ee091c
Travis CI <travis@testing-worker-linux-docker-6481a9e2-3463-linux-3.prod.travis-ci.org>
2016-07-22 05:23:00 +0000

    Auto-deploy from @/cheddar: SHA 0d18749d51280484f2ea5399cdc3d6b8b9999720; npm [npmignore]: Added npmignore file for bin

--- repl/console.js
+++ repl/console.js
@@ -1,5 +1,5 @@
 /*global Chedz, _console*/
-var VERSION = "1.0.0-beta.18.4";
+var VERSION = "1.0.0-beta.18.5";
 window.onload = function() {
 
     document.getElementById("code").onclick = function() {
