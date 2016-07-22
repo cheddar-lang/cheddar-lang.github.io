3cf2da7462e989f6239a446fd9ff754115bd20f3
Travis CI <travis@testing-worker-linux-docker-bd1d353b-3665-linux-1.prod.travis-ci.org>
2016-07-22 04:33:24 +0000

    Auto-deploy from @/cheddar: SHA 87ff0994f29f3e5886b415ce444f596f13a2d6a0; Merge branch 'release-1.0.0' into develop

--- repl/console.js
+++ repl/console.js
@@ -1,5 +1,5 @@
 /*global Chedz, _console*/
-var VERSION = "1.0.0-beta.18";
+var VERSION = "1.0.0-beta.18.2";
 window.onload = function() {
 
     document.getElementById("code").onclick = function() {
