547c418cfc93f9a1608c3280a3db7890dbef543c
Travis CI <travis@testing-worker-linux-docker-13d13a8a-3438-linux-12.prod.travis-ci.org>
2016-08-03 19:20:08 +0000

    Auto-deploy from @/cheddar: SHA 983562e6fe6b98ffa43e17692d08b5196c4886c0; cli [repl]: `exit` command didn't exit

--- repl/console.js
+++ repl/console.js
@@ -1,5 +1,5 @@
 /*global Chedz, _console*/
-var VERSION = "1.0.0-beta.27";
+var VERSION = "1.0.0-beta.28";
 window.onload = function() {
 
     document.getElementById("code").onclick = function() {
