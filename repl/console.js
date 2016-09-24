4f99a0541ae2941311411b442cc6ad3eaaa46a74
Travis CI <travis@testing-worker-linux-docker-ae5e1756-3411-linux-8.prod.travis-ci.org>
2016-09-24 21:13:06 +0000

    Auto-deploy from @/cheddar: SHA 0c53299f7bc3c8c983904e43eb037b6cb84e55b3; version [bump]: Bumped version to dev pre v1.0.0-beta.50

--- repl/console.js
+++ repl/console.js
@@ -1,5 +1,5 @@
 /*global Chedz, _console*/
-var VERSION = "1.0.0-beta.49";
+var VERSION = "1.0.0-beta.50";
 window.onload = function() {
 
     document.getElementById("code").onclick = function() {
